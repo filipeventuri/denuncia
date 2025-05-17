f1c7f20235d2ab610bcde909bc63ca6b9a54a264
filipeventuri <filipeventuri@icloud.com>
2025-05-17 17:09:08 -0300

    test

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const sequelize = require("sequelize");
 const connection = new sequelize ('lgbt', 'root', '123456789', {
     host: 'localhost',
     dialect: 'mysql',
-    timezone: '-03:00'
+    timezone: '-03:00'  
 })
 
 module.exports = connection;
