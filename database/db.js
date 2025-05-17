b40b4d8e1a5476597e4f5c6493b1a731c3622513
filipeventuri <filipeventuri@icloud.com>
2025-05-17 16:35:01 -0300

    view changes

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const sequelize = require("sequelize");
 
-const connection = new sequelize ('delta', 'root', '123456789', {
+const connection = new sequelize ('lgbt', 'root', '123456789', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
